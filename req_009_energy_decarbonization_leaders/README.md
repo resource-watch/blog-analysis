e985e1d9fdf56bdef9aaa289ae0442321263bf27
amsnyder <amelia.marie.snyder@gmail.com>
2020-01-13 14:59:20 -0500

    req_009 initial commit

---
+++ req_009_energy_decarbonization_leaders/README.md
@@ -0,0 +1,17 @@
+## Energy Decarbonization Analysis
+This file describes the analysis that was done for the blog "6 Lessons on Energy Decarbonization from Countries Leading the Way," posted on [Insights](https://www.wri.org/blog) and [TheCityFix](https://thecityfix.com/).
+
+This analysis involves two calculations:
+1) **The national electricity consumption as portion of total primary energy consumption**
+    - This was done using the [Electricity Consumption](https://resourcewatch.org/data/explore/ene034-Electricity-Consumption) and [Energy Consumption](https://resourcewatch.org/data/explore/ene033-Energy-Consumption_1) datasets on [Resource Watch](https://resourcewatch.org/).
+    - Each country's electricity consumption was divided by its total energy consumption for each year that data was available.
+    - This calculation can be found in the "Electricity Consumption per Total Energy Consumption" section of the [Python script](https://github.com/resource-watch/blog-analysis/blob/master/req_009_energy_decarbonization_leaders/req_009_energy_decarbonization_leaders_analysis.py).
+
+2) **The national rate of improvement in energy intensity**
+    - This was done with the [Energy Intensity dataset](https://resourcewatch.org/data/explore/2c444596-2be3-4786-bdfc-24010f99b21e) on [Resource Watch](https://resourcewatch.org/).
+    - For each year of data, every country's annual rate of change in energy intensity from the previous year was calculated. 
+    - This calculation can be found in the "Annual Rate of Change for Energy Intensity" section of the [Python script](https://github.com/resource-watch/blog-analysis/blob/master/req_009_energy_decarbonization_leaders/req_009_energy_decarbonization_leaders_analysis.py).
+
+Please see the [Python script](https://github.com/resource-watch/blog-analysis/blob/master/req_009_energy_decarbonization_leaders/req_009_energy_decarbonization_leaders_analysis.py) for the full analysis.
+
+###### Note: This dataset analysis was done by [Amelia Snyder](https://www.wri.org/profile/amelia-snyder).
---
+++ req_009_energy_decarbonization_leaders/req_009_energy_decarbonization_leaders_analysis.py
@@ -0,0 +1,140 @@
+import os
+import urllib
+import pandas as pd
+import requests
+from carto.datasets import DatasetManager
+from carto.auth import APIKeyAuthClient
+
+# name of the folder containing this analysis script
+request_name = 'req_009_energy_decarbonization_leaders'
+
+# set the directory that you are working in with the path variable
+# you can use an environmental variable, as we did, or directly enter the directory name as a string
+# example: path = '/home/req_009_energy_decarbonization_leaders'
+dir = os.getenv('BLOG_DIR')+request_name
+#move to this directory
+os.chdir(dir)
+
+# create a new sub-directory within your specified dir called 'data'
+# this is where we will store the results of our analysis
+data_dir = 'data/'
+if not os.path.exists(data_dir):
+    os.mkdir(data_dir)
+
+#define function to upload final data to Carto
+def upload_to_carto(csv_loc):
+    print('Uploading processed data to Carto.')
+    #set up carto authentication using local variables for username (CARTO_WRI_RW_USER) and API key (CARTO_WRI_RW_KEY)
+    auth_client = APIKeyAuthClient(api_key=os.getenv('CARTO_WRI_RW_KEY'), base_url="https://{user}.carto.com/".format(user=os.getenv('CARTO_WRI_RW_USER')))
+    #set up dataset manager with authentication
+    dataset_manager = DatasetManager(auth_client)
+    #upload dataset to carto
+    dataset = dataset_manager.create(csv_loc)
+    print('Carto table created: {}'.format(os.path.basename(csv_loc).split('.')[0]))
+    #set dataset privacy to 'Public with link'
+    dataset.privacy = 'LINK'
+    dataset.save()
+    print('Privacy set to public with link.')
+
+'''
+Electricity Consumption per Total Energy Consumption
+'''
+## Pull in relevant datasets
+
+# enter Resource Watch dataset ID for Electricity Consumption data
+electricity_consumption_dataset_id = 'ene034-Electricity-Consumption'
+# create SQL query to pull all data from API
+query = 'SELECT * from {}'.format(electricity_consumption_dataset_id)
+sql_query = urllib.parse.quote(query)
+# generate url for API call
+url = 'https://api.resourcewatch.org/query?sql={}'.format(sql_query)
+# request data and turn it into a pandas dataframe, then pull only the columns that we are interested in
+r = requests.get(url)
+json_data = r.json()['data']
+electricity_df = pd.DataFrame.from_records(json_data)[['year', 'country', 'electricity_consumption_ktoe']]
+
+# enter Resource Watch dataset ID for Energy Consumption data
+energy_consumption_dataset_id = 'ene033-Energy-Consumption_1'
+# create SQL query to pull all data from API
+query = 'SELECT * from {}'.format(energy_consumption_dataset_id)
+sql_query = urllib.parse.quote(query)
+# generate url for API call
+url = 'https://api.resourcewatch.org/query?sql={}'.format(sql_query)
+# request data and turn it into a pandas dataframe, then pull only the columns that we are interested in
+r = requests.get(url)
+json_data = r.json()['data']
+energy_df = pd.DataFrame.from_records(json_data)[['year', 'country', 'energy_consumption_ktoe']]
+
+
+
+## Calculate Electricity Consumption per Total Energy Consumption
+
+# merge the electricity consumption dataframe with the energy consumption dataframe, matching each by country and year
+merged_df = electricity_df.merge(energy_df, left_on=['year', 'country'], right_on=['year', 'country'])
+# calculate the electricity consumption per total energy consumption by dividing the two columns
+merged_df['electricity_per_total_energy'] = merged_df.electricity_consumption_ktoe/merged_df.energy_consumption_ktoe
+
+## Save and upload results to Carto
+
+# set the name to save this table as
+sub_request_name = 'req_009a_electricity_per_total_energy'
+# save the results of this analysis to a csv
+csv_loc = data_dir+sub_request_name+'.csv'
+merged_df.to_csv(csv_loc, index=False)
+# upload this table to Carto
+upload_to_carto(csv_loc)
+
+'''
+Annual Rate of Change for Energy Intensity
+'''
+## Pull in relevant datasets
+
+# enter Resource Watch dataset ID for Energy Intensity data
+energy_intensity_dataset_id = '2c444596-2be3-4786-bdfc-24010f99b21e'
+# create SQL query to pull all data from API
+query = 'SELECT * from {}'.format(energy_consumption_dataset_id)
+sql_query = urllib.parse.quote(query)
+# generate url for API call
+url = 'https://api.resourcewatch.org/query?sql={}'.format(sql_query)
+# request data and turn it into a pandas dataframe, then pull only the columns that we are interested in
+r = requests.get(url)
+json_data = r.json()['data']
+energy_intensity_df = pd.DataFrame.from_records(json_data)[['country_name', 'country_code', 'energy_intensity', 'year']]
+
+## Calculate Annual Rate of Change for Energy Intensity
+
+# get list of years in the table, over which we will do this calculation
+years = energy_intensity_df.year.unique()
+# sort years so that they are in order
+years.sort()
+
+# create an empty dataframe to store the annual rate of change results
+final_cols = ['country_name', 'country_code', 'year', 'energy_intensity_annual_rate_of_change']
+final_df = pd.DataFrame(columns=final_cols)
+
+# calculate annual rate of change for each year's energy intensity data, starting with the second year available
+for year in years[1:]:
+    # get subset of data for year we are calculating
+    energy_intensity_df_current_year = energy_intensity_df[energy_intensity_df.year==year]
+    # get subset of data for the year before
+    energy_intensity_df_previous_year = energy_intensity_df[energy_intensity_df.year==year-1]
+    # merge the two years of data based on country
+    merged_df = energy_intensity_df_current_year.merge(energy_intensity_df_previous_year, left_on=['country_code', 'country_name'], right_on=['country_code', 'country_name'], suffixes=[year, year-1])
+    # rename the current year's column to just be 'year' (because that is the year we will use as the indicator)
+    merged_df = merged_df.rename(columns={'year'+str(year):'year'})
+    # calculate the annual rate of change (current year energy intensity - previous year energy intensity) /  previous year energy intensity
+    merged_df['energy_intensity_annual_rate_of_change'] = (merged_df['energy_intensity'+str(year)] - merged_df['energy_intensity'+str(year-1)])/merged_df['energy_intensity'+str(year-1)]
+    # append the relevant columns to the final dataframe which will store all of the annual rate of change results
+    final_df=final_df.append(merged_df[final_cols], ignore_index=True)
+
+
+## Save and upload results to Carto
+
+# set the name to save this table as
+sub_request_name = 'req_009b_annual_rate_of_change_for_energy_intensity'
+# save the results of this analysis to a csv
+csv_loc = data_dir+sub_request_name+'.csv'
+merged_df.to_csv(csv_loc, index=False)
+# upload this table to Carto
+upload_to_carto(csv_loc)
+
