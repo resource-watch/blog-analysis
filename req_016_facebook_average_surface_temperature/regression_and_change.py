a7ab0235f402d119241e3e05aff79fce238f8215
Kristine Lister <31804766+kristinelister@users.noreply.github.com>
2020-09-04 09:46:57 -0400

    Add files via upload

--- req_016_facebook_average_surface_temperature/regression_and_change.py
+++ req_016_facebook_average_surface_temperature/regression_and_change.py
@@ -13,43 +13,50 @@ country_file_list = ['FRA_adm0_annual.csv','DEU_adm0_annual.csv','GBR_adm0_annua
                         'USA_adm0_annual.csv','USA_adm0_contiguous_annual.csv','USA_adm1_annual.csv',
                         'GBR_adm1_annual.csv','DEU_adm1_annual.csv','FRA_adm1_annual.csv']
 
+#Define years for linear regression
 years = np.arange(1950,2020)
 years_1970 = np.arange(1970,2020)
 
 for country_file in country_file_list:
     country_df = pd.read_csv(country_file)
     
+    #Separate attribute columns and data
     attribute_columns = [x for x in list(country_df) if x not in map(str, years)]
     out_df = country_df.copy()[attribute_columns]
     
+    #Get temperature columns and data
     temperature_columns = [x for x in list(country_df) if x in map(str, years)]
     temperature_data = country_df[temperature_columns]
     
     
     for i,row in temperature_data.iterrows():
-        #1950 - 2019 change
-        change = row['2019'] - row['1950']
-        out_df.at[i,'Change_1950_2019'] = change
+        #Calculate change in average temperature over different time period lengths
+        change_30 = np.mean(row[[str(x) for x in np.arange(1989,2020)]])-np.mean(row[[str(x) for x in np.arange(1950,1981)]])
+        change_10 = np.mean(row[[str(x) for x in np.arange(2009,2020)]])-np.mean(row[[str(x) for x in np.arange(1970,1981)]])
+        change_5 = np.mean(row[[str(x) for x in np.arange(2014,2020)]])-np.mean(row[[str(x) for x in np.arange(1970,1976)]])
         
+        out_df.at[i,'30_year_diff_1989_2019-1950_1980'] = change_30
+        out_df.at[i,'10_year_diff_2009_2019-1970_1980'] = change_10
+        out_df.at[i,'5_year_diff_2014_2019-1970_1975'] = change_5
+        
+        
+        #Get list of values for linear regression
         row_temp_data = row.values
-        
+        #Fit linear regression
         model = LinearRegression().fit(years.reshape((-1, 1)), row_temp_data)
-        
+        #Save values
         out_df.at[i,'LinReg_1950_2019_Intercept'] = model.intercept_        
         out_df.at[i,'LinReg_1950_2019_Slope'] = model.coef_[0]
         out_df.at[i,'LinReg_1950_2019_R_Squared'] = model.score(years.reshape((-1, 1)), row_temp_data)
         
-        
-        #1970 - 2019 change
-        change = row['2019'] - row['1970']
-        out_df.at[i,'Change_1970_2019'] = change
-        
+        #Get list of values for linear regression
         row_temp_data = row[map(str, years_1970)].values
-        
+        #Fit linear regression
         model = LinearRegression().fit(years_1970.reshape((-1, 1)), row_temp_data)
-        
+        #Save values
         out_df.at[i,'LinReg_1970_2019_Intercept'] = model.intercept_        
         out_df.at[i,'LinReg_1970_2019_Slope'] = model.coef_[0]
         out_df.at[i,'LinReg_1970_2019_R_Squared'] = model.score(years_1970.reshape((-1, 1)), row_temp_data)
-        
+    
+    #Save results
     out_df.to_csv(country_file.replace('annual','annual_regression'),index=False)
